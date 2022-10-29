a7c17e261171027ed661c1e3bb0d6e30a54ce0b6
Tars-knock <sfly13344@163.com>
2022-10-29 16:03:57 +0800

    init

---
+++ hello-eureka/hello-eureka/src/main/java/cn/tarsknock/helloeureka/HelloEurekaApplication.java
@@ -0,0 +1,13 @@
+package cn.tarsknock.helloeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloEurekaApplication.class, args);
+	}
+
+}
---
+++ hello-eureka/hello-eureka/src/test/java/cn/tarsknock/helloeureka/HelloEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tarsknock.helloeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
