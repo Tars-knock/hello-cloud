39bdc8dbb73061cd946cd9b5e8a72beb802a3546
Tars-knock <sfly13344@163.com>
2022-11-14 00:44:16 +0800

    update:consul client

--- hello-eureka/src/main/java/cn/tarsknock/helloeureka/HelloEurekaApplication.java
+++ hello-eureka/src/main/java/cn/tarsknock/helloeureka/HelloEurekaApplication.java
@@ -2,14 +2,18 @@ package cn.tarsknock.helloeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaServer
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HelloEurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloEurekaApplication.class, args);
+		new SpringApplicationBuilder(
+				HelloEurekaApplication.class)
+				.web(true).run(args);
 	}
 
 }
