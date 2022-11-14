734aff370cb03e0d056d57402716d6641bfff070
Tars-knock <sfly13344@163.com>
2022-11-15 00:45:38 +0800

    add:consul consumer

---
+++ eureka-consumer/src/main/java/cn/tarsknock/eurekaconsumer/controller/DcController.java
@@ -0,0 +1,28 @@
+package cn.tarsknock.eurekaconsumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+public class DcController {
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer")
+    public String dc(){
+        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-server");
+        String url = "http://"+serviceInstance.getHost()+":"+ serviceInstance.getPort() + "/dc";
+        System.out.println(url);
+        return restTemplate.getForObject(url, String.class);
+    }
+
+}
